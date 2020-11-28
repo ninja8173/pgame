27727677d9c26b8d2f12a715c69a67b787dd0fb8
ninja8173 <68139396+ninja8173@users.noreply.github.com>
2020-11-28 16:34:02 +0900

    Add files via upload

---
+++ ttttt.py
@@ -0,0 +1,151 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = SCREEN_HEIGHT = 450
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+CELL_SIZE = 150
+ROW_COUNT = SCREEN_HEIGHT // 3
+COL_COUNT = SCREEN_WIDTH // 3
+GRAY = (200, 200, 200)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+# 화면 설정
+pygame.display.set_caption("pyttt")
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+#보드 데이터
+TTTData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+Turn = 'x'
+
+#돌 모양 설정하기
+pygame.font.init()
+turn_font = pygame.font.SysFont('', 200)
+result_font = pygame.font.SysFont("arial black", 70)
+turn_x = turn_font.render('x', True, YELLOW)
+turn_o= turn_font.render('o', True, YELLOW)
+win_x = result_font.render("WINNER X", True, WHITE)
+win_o = result_font.render("WINNER O", True, WHITE)
+
+#격자 그리기
+def draw_grid():
+    for row in range(ROW_COUNT):
+        for col in range(COL_COUNT):
+            one_rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, GRAY, one_rect, 3)
+            #pygame.draw.rect(화면, 색상, 도형, 선 굵기)
+
+#클릭 좌표들 모드 인덱스로 변환
+def cell_to_board(m_pos):
+    for x in range(3):
+        if CELL_SIZE * x < m_pos[0] <= CELL_SIZE * x + CELL_SIZE:
+            cb_col = x
+    for y in range(3):
+        if CELL_SIZE * y < m_pos[1] <= CELL_SIZE * y + CELL_SIZE:
+            cb_row = y
+    return cb_row, cb_col
+
+#돌 모양 바꾸기
+def change_turn():
+    global Turn
+    if Turn == 'x':
+        Turn = 'o'
+    else:
+        Turn = 'x'
+
+#보드 그리기
+def print_mark(p_row, p_col):
+    turn_mark_rect = turn_o.get_rect().size
+    p_x = p_col * CELL_SIZE + CELL_SIZE // 2 - turn_mark_rect[0] // 2
+    p_y = p_row * CELL_SIZE + CELL_SIZE // 2 - turn_mark_rect[1] // 2
+    if Turn == 'x':
+        screen.blit(turn_x, (p_x, p_y))
+    else:
+        screen.blit(turn_o, (p_x, p_y))
+
+    #TTTData 모든 값을 보드에 출력합니다.
+    # screen.blit(turn_x, (x, y))
+
+#승자 판별 하기
+def check_winner(cw_row, cw_col):
+    global running
+    winner = False
+    if TTTData[cw_row][0] == TTTData[cw_row][1] == TTTData[cw_row][2] == Turn:
+        winner = True
+    if TTTData[0][cw_col] == TTTData[1][cw_col] == TTTData[2][cw_col] == Turn:
+        winner = True
+    if cw_row - cw_col == 0:
+        if TTTData[0][0] == TTTData[1][1] == TTTData[2][2] == Turn:
+            winner = True
+    if cw_row - cw_col == 0 or abs(cw_row - cw_col) == 2:
+        if TTTData[2][0] == TTTData[1][1] == TTTData[0][2] == Turn:
+            winner = True
+    if winner:
+        print_winner()
+        py_main()
+
+def reset_data():
+    global  TTTData, Turn
+    TTTData = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    Turn = 'x'
+    screen.fill(BLACK)
+    pygame.display.update()
+
+def print_winner():
+    result_surface = pygame.Surface( (SCREEN_WIDTH, SCREEN_HEIGHT))
+    result_surface.set_alpha(200)
+    result_surface.fill(BLACK)
+    screen.blit(result_surface, (0, 0))
+
+    win_rect = win_o.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    if Turn == 'o':
+        screen.blit(win_o, win_rect)
+    else:
+        screen.blit(win_x, win_rect)
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+    reset_data()
+    py_main()
+
+running = True
+
+#게임 루프
+running = True
+def py_main():
+    global running
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                row, col = cell_to_board(mouse_pos)
+                if TTTData[row][col] == 0:
+                    TTTData[row][col] = Turn
+                    print(TTTData)
+                    print_mark(row, col)
+                    check_winner(row, col)
+                    change_turn()
+
+        #격자 그리기
+        draw_grid()
+
+        #화면 업데이트
+        pygame.display.update()
+
+#파이게임 종료
+    pygame.quit()
+
+py_main()
